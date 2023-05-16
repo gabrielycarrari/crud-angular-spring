17413a9c1c321f9e015694146aad49d219fcf1bc
Gabriely Carrari <gabymcarrari@gmail.com>
2023-05-16 16:57:55 -0300

    add services

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 
 @Component({
@@ -10,15 +11,16 @@ import { Course } from '../model/course';
 
 export class CoursesComponent implements OnInit{
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'}
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name', 'category'];
 
-  constructor() {
+  constructor(private coursesServive: CoursesService) {
     // this.courses = [];
+    // this.coursesService = new CoursesService();
+    
   }
 
   ngOnInit(): void {
+    this.courses = this.coursesServive.list();
   }
 }
