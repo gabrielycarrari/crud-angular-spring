5cc1bfc82cbec38993b7e0b56fa16192eb8c3e1f
Gabriely Carrari <gabymcarrari@gmail.com>
2023-05-16 22:37:49 -0300

    alter table courses + add spinner + error handling

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 
@@ -6,21 +10,35 @@ import { CoursesService } from '../services/courses.service';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.scss']
 })
 
 export class CoursesComponent implements OnInit{
 
-  courses: Course[] = [];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesServive: CoursesService) {
+  constructor(
+    private coursesServive: CoursesService,
+    public dialog: MatDialog
+    ) {
     // this.courses = [];
     // this.coursesService = new CoursesService();
-    
+    this.courses$ = this.coursesServive.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos.');
+        return of([])
+      })
+    )
+  }
+  onError(errorMsg : string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
-    this.courses = this.coursesServive.list();
+
   }
 }
