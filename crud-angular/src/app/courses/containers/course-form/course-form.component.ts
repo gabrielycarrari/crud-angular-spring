ed3b5257ebb596ccabbc99efba1b1b9bd17084f2
Gabriely Carrari <gabymcarrari@gmail.com>
2023-05-24 10:48:44 -0300

    add error action

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -25,7 +25,6 @@ export class CourseFormComponent implements OnInit {
     private snackBar: MatSnackBar,
     private location: Location,
     private route: ActivatedRoute) {
-    //this.form
   }
 
   ngOnInit(): void {
