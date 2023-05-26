a23a97d02257215106fe8d21148cec2108832e1a
Gabriely Carrari <gabymcarrari@gmail.com>
2023-05-25 22:30:59 -0300

    update spring version

--- crud-spring/src/main/java/com/gabriely/crudspring/repository/CourseRepository.java
+++ crud-spring/src/main/java/com/gabriely/crudspring/repository/CourseRepository.java
@@ -1,11 +1,9 @@
 package com.gabriely.crudspring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.gabriely.crudspring.model.Course;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
     
 }
